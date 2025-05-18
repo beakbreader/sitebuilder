bd1ff77ed25f4d7f77d9ef950e97438964cfcc54
beakbreader <jimmy4374@outlook.com>
2025-05-18 19:48:50 +1000

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,7 @@
+// Replace these with your values from Supabase
+const SUPABASE_URL = 'https://vuemwgrfvogtzmmiwleg.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1ZW13Z3Jmdm9ndHptbWl3bGVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1NTgzNjcsImV4cCI6MjA2MzEzNDM2N30.9ZBKu-ApgpbOqb26kuCs98Gyzf-RaHzbi4zzWsczE9E';
+
+// Initialize Supabase client
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
